85d3e9e90e1e101a3c3401fef2c3a5ab528bb08a
Hikaruweng <hikaruweng@Hikaruwengs-MacBook-Pro.local>
2021-02-24 11:36:33 +0700

    Finish Homework3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
